36339d05a63220d83d3720ae034eab1bd0d8ebdf
paikpaik <woduf9311@gmail.com>
2023-09-13 14:26:50 +0900

    Fix: namespace 별 socket.js 수정

--- socket.js
+++ socket.js
@@ -2,22 +2,23 @@ const SocketIO = require("socket.io");
 
 module.exports = (server) => {
   const io = SocketIO(server, { path: "/socket.io" });
+  app.set("io", io);
+  const room = io.of("/room");
+  const chat = io.of("/chat");
 
-  io.on("connection", (socket) => {
-    const req = socket.request;
-    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-
-    console.log("새로운 클라이언트 접속", ip, socket.id);
+  room.on("connection", (socket) => {
+    console.log("room 네임스페이스 접속");
     socket.on("disconnect", () => {
-      console.log("클라이언트 접속 해제", ip, socket.id);
-      clearInterval(socket.interval);
+      console.log("room 네임스페이스 접속 해제");
     });
-    socket.on("reply", (data) => {
-      console.log(data);
+  });
+  chat.on("connection", (socket) => {
+    console.log("chat 네임스페이스 접속");
+    socket.on("join", (data) => {
+      socket.join(data); // 방에 참가
     });
-    socket.on("error", console.error);
-    socket.interval = setInterval(() => {
-      socket.emit("news", "Hello Socket.IO");
-    }, 3000);
+    socket.on("disconnect", () => {
+      console.log("chat 네임스페이스 접속 해제");
+    });
   });
 };
