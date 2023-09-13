d9bfcc630f6669a82012d0e65360f21ef492cfee
paikpaik <woduf9311@gmail.com>
2023-09-13 16:32:32 +0900

    Fix: session 분리 후 session을 활용한 user 코드 추가

--- app.js
+++ app.js
@@ -27,17 +27,18 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  })
-);
+
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+});
+app.use(sessionMiddleware);
+
 app.use((req, res, next) => {
   if (!req.session.color) {
     const colorHash = new ColorHash();
@@ -65,4 +66,4 @@ const server = app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
 });
 
-webSocket(server, app);
+webSocket(server, app, sessionMiddleware);
--- socket.js
+++ socket.js
@@ -1,10 +1,13 @@
 const SocketIO = require("socket.io");
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: "/socket.io" });
   app.set("io", io);
   const room = io.of("/room");
   const chat = io.of("/chat");
+  const wrap = (middleware) => (socket, next) =>
+    middleware(socket.request, {}, next);
+  chat.use(wrap(sessionMiddleware));
 
   room.on("connection", (socket) => {
     console.log("room 네임스페이스 접속");
@@ -16,9 +19,27 @@ module.exports = (server, app) => {
     console.log("chat 네임스페이스 접속");
     socket.on("join", (data) => {
       socket.join(data); // 방에 참가
+      socket.to(data).emit("join", {
+        user: "system",
+        chat: `${socket.request.session.color}님이 입장하셨습니다.`,
+      });
     });
-    socket.on("disconnect", () => {
+    socket.on("disconnect", async () => {
       console.log("chat 네임스페이스 접속 해제");
+      const { referer } = socket.request.headers;
+      const roomId = new URL(referer).pathname.split("/").at(-1);
+      const currentRoom = chat.adapter.rooms.get(roomId);
+      const userCount = currentRoom?.size || 0;
+      if (userCount === 0) {
+        await removeRoom(roomId);
+        room.emit("removeRoom", roomId);
+        console.log("방 제거 요청 성공");
+      } else {
+        socket.to(roomId).emit("exit", {
+          user: "system",
+          chat: `${socket.request.session.color}님이 퇴장하셨습니다.`,
+        });
+      }
     });
   });
 };
