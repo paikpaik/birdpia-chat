e11e6d8140ffd5516765b9e5468a409e2c3a4832
paikpaik <woduf9311@gmail.com>
2023-09-13 15:51:53 +0900

    Add: chat room 생성 route, controller 추가

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 const SocketIO = require("socket.io");
 
-module.exports = (server) => {
+module.exports = (server, app) => {
   const io = SocketIO(server, { path: "/socket.io" });
   app.set("io", io);
   const room = io.of("/room");
