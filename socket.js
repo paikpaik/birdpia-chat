e0933dd9d7fadcb520f7870110d683b043866234
paikpaik <woduf9311@gmail.com>
2023-09-13 12:38:55 +0900

    socket.io 모듈

--- socket.js
+++ socket.js
@@ -1,23 +1,23 @@
-const WebSocket = require("ws");
+const SocketIO = require("socket.io");
 
 module.exports = (server) => {
-  const wss = new WebSocket.Server({ server });
+  const io = SocketIO(server, { path: "/socket.io" });
 
-  wss.on("connection", (ws, req) => {
+  io.on("connection", (socket) => {
+    const req = socket.request;
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    console.log("새로운 클라이언트 접속", ip);
-    ws.on("message", (message) => {
-      console.log(message.toString());
+
+    console.log("새로운 클라이언트 접속", ip, socket.id);
+    socket.on("disconnect", () => {
+      console.log("클라이언트 접속 해제", ip, socket.id);
+      clearInterval(socket.interval);
     });
-    ws.on("error", console.error);
-    ws.on("close", () => {
-      console.log("클라이언트 접속 해제", ip);
-      clearInterval(ws.interval);
+    socket.on("reply", (data) => {
+      console.log(data);
     });
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
-      }
+    socket.on("error", console.error);
+    socket.interval = setInterval(() => {
+      socket.emit("news", "Hello Socket.IO");
     }, 3000);
   });
 };
